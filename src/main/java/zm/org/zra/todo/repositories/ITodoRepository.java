5061ee1ee9014b7e8b43b00f7705506db4aa89d5
izzyjere <wisdomjere5@gmail.com>
2023-04-05 10:53:27 +0200

    clean up

--- src/main/java/zm/org/zra/todo/repositories/ITodoRepository.java
+++ src/main/java/zm/org/zra/todo/repositories/ITodoRepository.java
@@ -1,12 +1,10 @@
 package zm.org.zra.todo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import zm.org.zra.todo.models.Todo;
 
 import java.util.List;
 
-@Repository
 public interface ITodoRepository extends JpaRepository<Todo,Long> {
     List<Todo> findAllByTodoUserId(Long userid);
 }
--- src/main/java/zm/org/zra/todo/repositories/IUserRepository.java
+++ src/main/java/zm/org/zra/todo/repositories/IUserRepository.java
@@ -1,12 +1,10 @@
 package zm.org.zra.todo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import zm.org.zra.todo.models.TodoUser;
 
 import java.util.Optional;
 
-@Repository
 public interface IUserRepository extends JpaRepository<TodoUser,Long> {
    Optional<TodoUser> findByUsername(String username);
 }
